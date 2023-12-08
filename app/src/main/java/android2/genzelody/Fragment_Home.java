35d2b2cc205bdb6d0646832056aa6d5e7dc4a966
CRCAT25 <136076219+CRCAT25@users.noreply.github.com>
2023-12-08 12:10:47 +0700

    detail playlist

--- app/src/main/java/android2/genzelody/Fragment_Home.java
+++ app/src/main/java/android2/genzelody/Fragment_Home.java
@@ -167,11 +167,10 @@ public class Fragment_Home extends Fragment implements RecyclerViewClickListener
     @Override
     public void onClick(View view, int position, String category) {
         if(category.equals("myplaylist")){
-            loadFragment(new fragment_detail_playlist(MyPlayList.get(position)));
+            loadFragment(new fragment_detail_playlist(MyPlayList.get(position), RecommendedTrackList));
         } else if (category.equals("feature")) {
-            loadFragment(new fragment_detail_playlist(FeaturePlayList.get(position)));
+            loadFragment(new fragment_detail_playlist(FeaturePlayList.get(position), RecommendedTrackList));
         }else{
-
             loadFragment(new Fragment_Play_Track(RecommendedTrackList,"Danh sách phổ biến", position));
         }
     }
